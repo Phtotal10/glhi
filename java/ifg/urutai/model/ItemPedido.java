06f344e28c88357d9f193945f8c9bf1f017e1129
Phtotal10 <120691238+Phtotal10@users.noreply.github.com>
2023-11-08 20:45:39 -0300

    Add files via upload

---
+++ java/ifg/urutai/model/ItemPedido.java
@@ -0,0 +1,34 @@
+package ifg.urutai.model;
+
+public class ItemPedido {
+	
+	private int id;
+	private int quantidade;
+	private Pedido pedido;
+	private Produto produto;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Pedido getPedido() {
+		return pedido;
+	}
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+}
